ea908101d1f0c2befef08ee5b70586571c65aac8
cengiztuglu <cngztuglu@gmail.com>
2023-12-31 19:18:13 +0300

    NavigateComment

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import './ListItem.css'
 
-const ListItem = ({title, description, price, itemImage}) => {
+
+const ListItem = ({ title, description, price, itemImage }) => {
+  const [showCommentForm, setShowCommentForm] = useState(false);
+  const navigate = useNavigate();
+
+  
+  const handleCommentClick = () => {
+    setShowCommentForm(true);
+    navigate('/Comment'); // Yorum yapılacak sayfa rotası buraya gelecek
+
+  };
+
   return (
     <div className='list-items'>
-        <ul>
-            <li>
-                {itemImage && <img src={`data:image/jpeg;base64,${itemImage}`} alt="hero" />}
-                
-                <div>
-                    <h3>{title}</h3>
-                    {description && <p>{description}</p>}
-                </div>
+      <ul>
+        <li>
+          {itemImage && <img src={`data:image/jpeg;base64,${itemImage}`} alt="hero" />}
+          
+          <div>
+            <h3>{title}</h3>
+            {description && <p>{description}</p>}
+          </div>
 
-                <h3>{price} TL</h3>
-            </li>
-        </ul>
+          <h3>{price} TL</h3>
+          <p onClick={handleCommentClick} className="comment-text">Yorum Yap</p>
+          {/* Yorum yapma formu */}
+          {showCommentForm && (
+            <div>
+              {/* Yorum yapma formu veya modal */}
+              {/* ... */}
+            </div>
+          )}
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ListItem
+export default ListItem;
